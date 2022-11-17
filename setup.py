76310e42eb5399d1fdec72d4abf802301d27137c
Mateusz Leonowicz <mleonowicz@internships.antmicro.com>
2022-11-17 15:33:15 +0100

    Fix package name in setup.py
    Signed-off-by: Mateusz Leonowicz <mleonowicz@internships.antmicro.com>

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('README.md', 'r') as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='pipeline_manager_communication_backend',
+    name='kenning_pipeline_manager_backend_communication',
     version='0.1',
     packages=setuptools.find_packages(),
     long_description=long_description,
