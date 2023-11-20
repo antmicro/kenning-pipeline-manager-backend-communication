5f86f0571df5d662247a0fee71c176c1cc205dcd
Eryk Szpotanski <eszpotanski@antmicro.com>
2023-11-20 15:05:04 +0100

    [#51350] __init__: Use uvloop for event loop
    Signed-off-by: Eryk Szpotanski <eszpotanski@antmicro.com>

--- pipeline_manager_backend_communication/__init__.py
+++ pipeline_manager_backend_communication/__init__.py
@@ -8,3 +8,8 @@ Main module of the backend communication.
 This module should be used to communicate between Pipeline Manager application
 and other Python based applications.
 """
+
+import asyncio
+import uvloop
+
+asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
